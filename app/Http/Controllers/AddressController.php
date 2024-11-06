fba32a3e335fcba186a4a1cca947f5f9789f112a
JuanJoseRomeroMontes <claseinfojuanjose@gmail.com>
2024-11-06 19:49:23 +0100

    feat: create address migration and model

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Address $address)
+    {
+        //
+    }
+}
