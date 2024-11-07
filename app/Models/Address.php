6624cae2b1cbc2a88c4c3d6d2c21dd1da6ba9a49
Juan José Romero Montes <141632840+JuanJoseRomeroMontes@users.noreply.github.com>
2024-11-07 16:25:27 +0100

    Feature/db migrations (#4)
    * Fix: added config file to gitignore
    
    * feat: create address migration and model
    
    * docs: document migration commands on README
    
    * docs: document address model and controller

---
+++ app/Models/Address.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Address model
+ *
+ * PHP version 8
+ *
+ * @category Model
+ * @package  Model\Address
+ * @author   Juan José Romero <claseinfojuanjose@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/jvngarcia/devathon-8-backend.git
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'address'
+    ];
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
