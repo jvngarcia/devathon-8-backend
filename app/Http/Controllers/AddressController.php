72a218daf5b658bf90023fe8ce66b69ce43ba56d
Angel García <angelgarciaweb@gmail.com>
2024-11-08 11:22:40 +0100

    feat: add endpoint and documentation enhancements

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -13,6 +13,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\AddressNotFoundException;
 use App\Http\Resources\AddressCollection;
 use App\Models\Address;
 use Illuminate\Http\JsonResponse;
@@ -26,15 +27,6 @@ use Illuminate\Http\Request;
  * @author   Juan José Romero <claseinfojuanjose@gmail.com>
  * @license  MIT https://opensource.org/licenses/MIT
  * @link     https://github.com/jvngarcia/devathon-8-backend.git
- *
- * @OA\Info(
- *     title="Devathon 8: Team 1",
- *     version="0.1",
- * ),
- * @OA\Server(
- *      description="Localhost",
- *      url="http://localhost:8000/api/"
- * ),
  */
 class AddressController extends Controller
 {
@@ -54,12 +46,27 @@ class AddressController extends Controller
      * @OA\Get(
      *     path="/v1/addresses/recent",
      *     summary="Get the last 5 addresses",
+     * @OA\Parameter(
+     *         name="X-API-Key",
+     *         in="header",
+     *         description="API Key",
+     * @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
      * @OA\Response(response=200, description="Successful operation"),
+     * @OA\Response(response=404, description="Not found"),
      * @OA\Response(response=401, description="Unauthorized"),
      *  )
      */
     public function show(): AddressCollection
     {
-        return new AddressCollection(Address::orderBy('created_at', 'desc')->limit(4)->get());
+        $data = Address::orderBy('created_at', 'desc')->limit(5)->get();
+
+        if ($data->isEmpty()) {
+            throw new AddressNotFoundException();
+        }
+
+        return new AddressCollection($data);
     }
 }
--- app/Models/Address.php
+++ app/Models/Address.php
@@ -16,19 +16,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Address model
+ *
+ * @category Model
+ * @package  Model\Address
+ * @author   Juan José Romero <claseinfojuanjose@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/jvngarcia/devathon-8-backend.git
+ */
 class Address extends Model
 {
     use HasFactory;
+
     /**
-    * The attributes that are mass assignable.
-    *
-    * @var array<string>
-    */
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
     protected $fillable = [
         'address'
     ];
 
     /**
+     * Define primary key
+     *
      * @var string
      */
     protected $primaryKey = 'id';
