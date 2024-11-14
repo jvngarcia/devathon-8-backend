a1ec8e58e13519b095b362b077bb54907b1c5062
dajramirez <79631800+dajramirez@users.noreply.github.com>
2024-11-14 22:56:13 +0100

    Addresses (#8)
    * feat: /adresses Route created
    
    * feat: store function with error return if the address already exists in the database
    
    * feat: store function comments and choose 409 error as the "Address already exists" error
    
    * feat: function store complete included documentation and AddressAlreadyExistsException.php created
    
    * fix: error when trying to save data
    
    * docs: update swagger
    
    * mensaje no válido
    
    * docs: fixing paths
    
    * fix: change 'city' and 'country' validation from 'required' to 'nullable'
    
    * docs: change of 'city' and 'country' validation from 'required' to 'nullable'
    
    ---------
    
    Co-authored-by: Angel García <angelgarciaweb@gmail.com>

---
+++ app/Exceptions/AddressAlreadyExistsException.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * AddressAlreadyExistsException
+ * 
+ * PHP version 8
+ * 
+ * @category Exceptions
+ * @package  Exceptions\Address
+ * @author   Darío Jesús Ramírez Romero <dariojesusramirez@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/jvngarcia/devathon-8-backend.git
+ */
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class AddressAlreadyExistsException extends Exception
+{
+  /**
+   * Render the exception as an HTTP response.
+   *
+   * @return Json
+   */
+  public function render(): JsonResponse
+  {
+    return response()->json(
+      [
+        'errors' => [
+          [
+            'status' => 422,
+            'title' => 'Unprocessable Entity',
+            'detail' => 'Address already exists.',
+          ],
+        ],
+      ],
+      422
+    );
+  }
+}
--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Address Controller
  *
@@ -14,10 +15,12 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\AddressNotFoundException;
+use App\Exceptions\AddressAlreadyExistsException;
 use App\Http\Resources\AddressCollection;
 use App\Models\Address;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 /**
  * Address Controller
@@ -32,10 +35,123 @@ class AddressController extends Controller
 {
     /**
      * Store a newly created resource in storage.
+     *
+     * This method validates the incoming request data, checks if the address already exists, and creates a new address record if it does not.
+     *
+     * @param Request $request The incoming request containing the address data.
+     *
+     * @return JsonResponse A JSON response indicating the success or failure of the address creation.
+     *
+     *
+     * @OA\Post(
+     *     path="/v1/addresses",
+     *     summary="Create a new address",
+     * @OA\Parameter(
+     *         name="X-API-Key",
+     *         in="header",
+     *         description="API Key",
+     * @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     tags={"Addresses"},
+     * @OA\RequestBody(
+     *         required=true,
+     *         description="Address data",
+     * @OA\JsonContent(
+     *           required={"place", "coordinates"},
+     *           nullable={"city", "country"},
+     * @OA\Property(
+     *             property="place",
+     *             type="string",
+     *             example="My house"
+     *           ),
+     * @OA\Property(
+     *            property="city",
+     *            type="string",
+     *            example="My city"
+     *  ),
+     * @OA\Property(
+     *            property="country",
+     *            type="string",
+     *            example="My country"
+     *   ),
+     * @OA\Property(
+     *            property="coordinates", type="object",
+     * @OA\Property(
+     *            property="longitude",
+     *            type="number",
+     *            format="float",
+     *            example="0.0"
+     *   ),
+     * @OA\Property(
+     *            property="latitude",
+     *            type="number",
+     *            format="float",
+     *            example="0.0"
+     *   )
+     *  )
+     *  )
+     * ),
+     * @OA\Response(
+     *           response=201,
+     *           description="Address created successfully",
+     *           @OA\JsonContent(
+     *           @OA\Property(
+     *        property="message",
+     *        type="string",
+     *        example="Address created successfully"
+     *        ),
+     *        @OA\Property(
+     *        property="address",
+     *        type="object",
+     *        )
+     *  )
+     * )
+     * )
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        try {
+            $validatedData = $request->validate([
+                'place' => 'required|string',
+                'city' => 'nullable|string',
+                'country' => 'nullable|string',
+                'coordinates.longitude' => 'required|numeric|min:-180|max:180',
+                'coordinates.latitude' => 'required|numeric|min:-90|max:90',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(
+                [
+                    'errors' => 'Invalid input data',
+                    'details' => $e->errors()
+                ],
+                400
+            );
+        }
+
+
+        if (Address::where('longitude', $validatedData['coordinates']['longitude'])
+            ->where('latitude', $validatedData['coordinates']['latitude'])
+            ->exists()
+        ) {
+            throw new AddressAlreadyExistsException();
+        }
+
+        $address = Address::create(
+            [
+                'place' => $validatedData['place'],
+                'city' => $validatedData['city'],
+                'country' => $validatedData['country'],
+                'longitude' => $validatedData['coordinates']['longitude'],
+                'latitude' => $validatedData['coordinates']['latitude']
+            ]
+        );
+
+        return response()->json([
+            'message' => 'Address created successfully',
+            'address' => $address
+        ], 201);
     }
 
     /**
@@ -46,6 +162,7 @@ class AddressController extends Controller
      * @OA\Get(
      *     path="/v1/addresses/recent",
      *     summary="Get the last 5 addresses",
+     *     tags={"Addresses"},
      * @OA\Parameter(
      *         name="X-API-Key",
      *         in="header",
