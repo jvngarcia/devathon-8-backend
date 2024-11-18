045248db962f404a5c155f3d17c49bc98afbbbd2
dajramirez <dariojesusramirez@gmail.com>
2024-11-18 11:02:03 +0100

    docs: model documentation

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Letter model
+ * 
+ * @category Model
+ * @package  Model\Letter
+ * @author   Darío Jesús Ramírez Romero <dariojesusramirez@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/jvngarcia/devathon-8-backend.git
+ */
+
 class Letter extends Model
 {
   use HasFactory;
