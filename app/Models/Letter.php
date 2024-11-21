dfe295e8d07e4a11956ffcf41364da46e61e3365
dajramirez <79631800+dajramirez@users.noreply.github.com>
2024-11-21 21:20:47 +0100

    Letters (#13)
    * ci: update .gitignore
    
    * ci: include required files to keep conventional commits
    
    * feat: create Letter model and make 'false' the default value for 'read'
    
    * feat: migration completed
    
    * docs: model documentation
    
    * fix: remove image from model and table
    
    * fix: include image_url as a fillable attribute in the model and as a nullable field in the table

---
+++ app/Models/Letter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Letter model
+ * 
+ * @category Model
+ * @package  Model\Letter
+ * @author   Darío Jesús Ramírez Romero <dariojesusramirez@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/jvngarcia/devathon-8-backend.git
+ */
+
+class Letter extends Model
+{
+  use HasFactory;
+
+  /** 
+   * The attributes that are mass assignable.
+   * 
+   * @var array<string>
+   */
+
+  protected $fillable = [
+    'sender',
+    'subject',
+    'content',
+    'read',
+    'image_url'
+  ];
+
+  /**
+   * The attributes that should be cast.
+   * 
+   * @var array
+   */
+
+  protected $casts = [
+    'read' => 'boolean',
+  ];
+
+  /**
+   * The model's default values for attributes.
+   * 
+   * @var array
+   */
+
+  protected $attributes = [
+    'read' => false,
+  ];
+}
